5fd5a1298fa86317a52d09c5052edadc40545697
stonelee <istonelee@gmail.com>
2012-12-20 13:45:21 +0800

    add loop voice

--- sound/main.js
+++ sound/main.js
@@ -1,37 +1,48 @@
-Ext.onReady(function () {
+Ext.onReady(function() {
+  // 闪烁
+  function blink(btn) {
+    blink.self = btn;
+  }
+  blink.start = function() {
+    blink.stop();
+    Ext.TaskManager.start(blink.task);
+  };
+  blink.stop = function() {
+    if (blink.running) {
+      Ext.TaskManager.stop(blink.task);
+    }
+    blink.running = false;
 
-  blink.flag = true;
+    blink.flag = true;
+    blink.self.setIconCls('icon_application_view_tile');
+  };
   blink.task = {
     run: function() {
-      blink.flag ? blink.self.setIcon('clock_red.png'): blink.self.setIcon('clock.png');
+      blink.self.setIconCls(blink.flag ? 'icon_clock_red' : 'icon_clock');
       blink.flag = !blink.flag;
+      blink.running = true;
     },
     interval: 300
   };
-  function blink(btn){
-    blink.self = btn;
-    Ext.TaskManager.start(blink.task);
-  }
-  blink.stop = function(){
-    blink.self.setIcon('application_view_tile.png');
-    blink.flag = true;
 
-    Ext.TaskManager.stop(blink.task);
-  };
+  // 声音
+  function voice() {}
+  voice.start = function(isLoop) {
+    voice.stop();
 
-  voice.self = null;
-  function voice(){
-    var file = 'mail';
+    var file = 'static-resources/mail';
+    var loop;
     if (Ext.isIE) {
-      //Ext.DomHelper方式创建无效
+      loop = isLoop === true ? 'infinite' : 1;
+      // Ext.DomHelper方式创建无效
       var sound = document.createElement('bgsound');
       sound.setAttribute('src', file + '.wav');
-      sound.setAttribute('loop', 1);
+      sound.setAttribute('loop', loop);
       sound.setAttribute('autostart', true);
       document.body.appendChild(sound);
       voice.self = sound;
     } else {
-      voice.self = Ext.DomHelper.append(document.body, {
+      var opts = {
         tag: 'audio',
         children: [{
           tag: 'source',
@@ -41,23 +52,24 @@ Ext.onReady(function () {
           tag: 'source',
           src: file + '.mp3',
           type: 'audio/mp3'
-        }]});
+        }]
+      };
+      if (isLoop === true) {
+        opts.loop = 'loop';
+      }
+
+      voice.self = Ext.DomHelper.append(document.body, opts);
       voice.self.play();
     }
-  }
-  voice.stop = function(){
+  };
+  voice.stop = function() {
     Ext.removeNode(voice.self);
   };
-
-  Ext.get('newMessage').on('click', function() {
-    var btn = Ext.getCmp('btn');
-    blink(btn);
-    voice();
-  });
+  voice.self = null;
 
   Ext.create('Ext.Button', {
     id: 'btn',
-    text: 'Click me',
+    text: 'stop',
     icon: 'application_view_tile.png',
     renderTo: Ext.getBody(),
     handler: function() {
@@ -65,4 +77,19 @@ Ext.onReady(function () {
       voice.stop();
     }
   });
+
+  //使用
+  var btn = Ext.getCmp('btn');
+  blink(btn);
+  voice();
+
+  Ext.get('start').on('click', function() {
+    blink.start();
+    voice.start();
+  });
+  Ext.get('loop').on('click', function() {
+    blink.start(true);
+    voice.start(true);
+  });
+
 });
